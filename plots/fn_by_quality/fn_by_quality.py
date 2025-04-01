b9ff43193dd79b6eefb0d90b0fe31ab8c090ef18
Hank Charlton <charlton@admins-MacBook-Pro.local>
2025-04-01 12:44:24 +0200

    modify fnbyquality plot to use lazy api

--- plots/fn_by_quality/fn_by_quality.py
+++ plots/fn_by_quality/fn_by_quality.py
@@ -4,25 +4,23 @@ import polars as pl
 import altair as alt
 alt.data_transformers.enable("vegafusion")
 
-df = pl.read_parquet(PROCESSED_DATA_DIR / 'sample_bam_null')#.filter(pl.col('center_qual')==[93,93,93])
 
-
-df = df.with_columns(
-    (pl.col('center_qual').list[0].alias('center_qual_0')),
-    (pl.col('center_qual').list[1].alias('center_qual_1')),
-    (pl.col('center_qual').list[2].alias('center_qual_2'))
-)
-
-
-df_passes_group = df.group_by('fn').agg(
-    pl.col('center_qual_1').var().alias('center_qual_1_var'),
-    pl.col('center_qual_1').mean().alias('center_qual_1_mean')
+q = (
+    pl.scan_parquet(PROCESSED_DATA_DIR / 'sample_bam_null')
+    .select('center_qual', 'fn')
+    .with_columns(
+        (pl.col('center_qual').list[0].alias('center_qual_0')),
+        (pl.col('center_qual').list[1].alias('center_qual_1')),
+        (pl.col('center_qual').list[2].alias('center_qual_2')))
+    .group_by('fn')
+    .agg(pl.col('center_qual_1').var().alias('center_qual_1_var'),
+         pl.col('center_qual_1').mean().alias('center_qual_1_mean'))
     )
 
+df = q.collect()
 
-chart_fn_variance = alt.Chart(df_passes_group).mark_circle().encode(
+alt.Chart(df).mark_circle().encode(
     alt.X('fn:Q'),
     alt.Y('center_qual_1_mean:Q')
-)
+).save('./qualityVar-fn.svg')
 
-chart_fn_variance.save('qualityVar-fn.svg')
