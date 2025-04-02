600a23623bbe82049ca91eefce4ec111dadc1477
Chester Henry Charlton <henrycharlton1@gmail.com>
2025-04-02 10:42:10 +0200

    update q93 percent plot

--- plots/qual_hist/plot.py
+++ plots/qual_hist/plot.py
@@ -11,7 +11,7 @@ q = (
     (pl.col('center_qual').list[1].alias('center_qual_1')),
     (pl.col('center_qual').list[2].alias('center_qual_2')))
     .group_by('fn')
-    .agg(((pl.col('center_qual_1') >= 93).sum()/pl.col('center_qual_1').count()).alias('percentGEQ93'))
+    .agg(((pl.col('center_qual_1') >= 90).sum()/pl.col('center_qual_1').count()).alias('percentGEQ93'))
 )
 
 df = q.collect()
@@ -20,9 +20,9 @@ chart = alt.Chart(df).mark_line().encode(
     alt.X('fn:Q'),
     alt.Y('percentGEQ93:Q')
 ).properties(
-    title='Percent greater than Q93 by passes',
+    title='Percent greater than Q90 by passes',
     width=500,
     height=500
 )
 
-chart.save('./percentGEQ93_fn.svg')
+chart.save('./percentGEQ90_fn.svg')
